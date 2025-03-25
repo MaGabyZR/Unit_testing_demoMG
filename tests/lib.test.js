9ca79ae6348f376bec9936a55af1c98874aee6db
Gabriela Rubio <gabyrubioa@gmail.com>
2025-03-25 11:24:34 -0600

    Grouping tests.

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -4,21 +4,24 @@ const lib = require('../lib');
   //throw new Error('Something failed miserably!');         //To produce an error and fail the test. 
 }); */
 
-//Testing numbers.
-//First test case.
-test('absolute - should return a positive number if input is positive', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
+//Group all related tests for testing numbers.
+describe('absolute', () => {
+    //First test case.
+    it('should return a positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+
+    //Second test case.
+    it('should return a positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+
+    //Third test case.
+    it('should return 0 if input is 0', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
 
-//Second test case.
-test('absolute - should return a positive number if input is negative', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
-
-//Third test case.
-test('absolute - should return 0 if input is 0', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
-});
